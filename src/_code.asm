69da842413a54ce5705953442a834463e468e7d3
zxwanderer <new@email>
2020-07-31 20:28:53 +0300

    add input routines

--- src/_code.asm
+++ src/_code.asm
@@ -1,4 +1,7 @@
 include "engine/lib/im2/im2_h.asm"
+include "engine/lib/keyboard/scancode_h.asm"
+include "src/middleware/view_h.asm"
+
 start:
     DI
 
@@ -26,32 +29,40 @@ start:
 
     EI
 loop:
-    LD A, 1
+    LD A, (Input.pressButtons)
+    ; AND %00000111
     OUT (#FE), A
-    LD A, 2
-    OUT (#FE), A
-    LD A, 3
-    OUT (#FE), A
-    LD A, 4
-    OUT (#FE), A
-    LD A, 5
-    OUT (#FE), A
-    LD A, 6
-    OUT (#FE), A
-    LD A, 7
-    OUT (#FE), A
+    LD HL, keytable
+    CALL Input.scanKeys
     JP loop
+
+keytable:
+  KEY_Q
+  defb BUTTON_UP
+  KEY_A
+  defb BUTTON_DOWN
+  KEY_O
+  defb BUTTON_LEFT
+  KEY_P
+  defb BUTTON_RIGHT
+  KEY_M
+  defb BUTTON_FIRE
+  KEY_SPACE
+  defb BUTTON_FIRE
+  KEY_ENTER
+  defb BUTTON_FIRE
+  defb 0
 
 include "engine/lib/screen/set_colors.asm"
 include "engine/lib/screen/calc_down_pos.asm"
 include "engine/lib/screen/clear.asm"
 include "engine/lib/screen/addr_to_attr.asm"
-; include "engine/lib/screen/flip.asm"
 
 include "engine/lib/tiles16/show.asm"
 include "engine/lib/tiles16/index_to_ptr.asm"
 
-include "src/middleware/view_h.asm"
+include "engine/lib/keyboard/input.asm"
+
 include "src/middleware/view.asm"
 
 include "engine/lib/memory/set_bank.asm"
