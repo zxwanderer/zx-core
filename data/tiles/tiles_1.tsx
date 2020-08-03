f18b0091d1a75070afa3592376de171aeb48d147
zxwanderer <new@email>
2020-08-03 10:47:59 +0300

    add some new map

---
+++ data/tiles/tiles_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tiles_1" tilewidth="16" tileheight="16" tilecount="192" columns="16">
+ <image source="tiles.png" width="256" height="192"/>
+</tileset>
