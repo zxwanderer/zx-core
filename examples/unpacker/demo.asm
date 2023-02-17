62dfc475d9f0ca0c89a9ccb44ead05f836fb9a73
zxwanderer <no@mail.com>
2023-02-17 19:37:07 +0300

    update packer

--- examples/unpacker/demo.asm
+++ examples/unpacker/demo.asm
@@ -6,21 +6,33 @@ start:
 	; LD BC, #2000
 	; ldir 
 
-	LD HL, picture_pack:
+	; LD HL, picture_pack:
+	; LD DE, #4000
+	; CALL Salvador.unpack
+
+	di
+
+	DEFINE  Upkr.buffer #F000
+
+	LD IX, picture_pack
 	LD DE, #4000
-	CALL Salvador.unpack
+  EXX
+	CALL Upkr.unpack
+	ei
 loop:
 	jp loop
 
 unpacker:
-	include "unpackers/salvador/unpack.asm"
+	include "unpackers/upkr/unpack.asm"
+	; include "unpackers/salvador/unpack.asm"
 
 picture_pack:
-	incbin "./output/prof4d_np_2015_CC_Winter.sal"
+	incbin "./output/prof4d_np_2015_CC_Winter.upkr"
+	; incbin "./output/prof4d_np_2015_CC_Winter.sal"
 ; picture:
 	; incbin "prof4d_np_2015_CC_Winter.scr"
 	
-	display 'PAGE0 end: ', $
+	display 'Program end: ', $
 	display /d, 'Total bytes used: ', $ - start
 
 	; build
