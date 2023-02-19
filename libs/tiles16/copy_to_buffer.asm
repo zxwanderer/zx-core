d5979025a90bc110a9c7decd2a96f96ac53ce36a
zxwanderer <no@mail.com>
2023-02-19 20:05:52 +0300

    update to map_mask

--- libs/tiles16/copy_to_buffer.asm
+++ libs/tiles16/copy_to_buffer.asm
@@ -1,6 +1,10 @@
 ; копируем тайлы из карты в массив отрисовки
 ; в HL указатель на индекс первого тайла карты
 COPY_TO_BUFFER:
+  LD DE, MAP_MASK-MAP_SET
+  ADD HL, DE
+  LD DE, MAP_SET-MAP_MASK
+
   LD IX, VIEW_BUFFER
   LD B, TILE_SCR_HEIGTH
 copy_loop2:
@@ -15,17 +19,20 @@ copy_loop1:
   AND %10000000
   JR Z, no_copy_cell
 
+  PUSH HL
+  ADD HL,DE
   LD A, (HL)
-  LD (real_cell_value), A
+  POP HL
 
-real_cell_value: equ $+1
-  LD A, #00
 no_copy_cell:
   LD (IX), A
+
   INC IX
   INC HL
+
   POP BC
   DJNZ copy_loop1
+
   POP HL
   LD BC, TILE_MAP_SIZE_WIDTH
   ADD HL, BC; прибавляем к указателю на начало тайлов ширину - сдвигаем указатель вниз на 1 тайл
@@ -33,3 +40,4 @@ no_copy_cell:
   POP BC
   DJNZ copy_loop2
   RET
+  
