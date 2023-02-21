d430939fdce499205b0451fa2c60009954492e34
zxwanderer <no@mail.com>
2023-02-21 22:59:54 +0300

    update

--- libs/view/view_h.asm
+++ libs/view/view_h.asm
@@ -1,4 +1,4 @@
 VIEW_SCR_WINDOW_MAX_X equ TILE_MAP_SIZE_WIDTH-TILE_SCR_WIDTH+1  ; максимальная позиция окна отображения карты, иначе выходим за границу
 VIEW_SCR_WINDOW_MAX_Y equ TILE_MAP_SIZE_HEIGHT-TILE_SCR_HEIGTH+1 ; максимальная позиция окна отображения карты, иначе выходим за границу
-VIEW_SCR_WIDTH_HALF equ scrWidth/2;  8
-VIEW_SCR_HEIGHT_HALF equ scrHeight/2; 6
+VIEW_SCR_WIDTH_HALF equ TILE_SCR_WIDTH/2;  8
+VIEW_SCR_HEIGHT_HALF equ TILE_SCR_HEIGTH/2; 6
