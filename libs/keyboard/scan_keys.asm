780800b86123c09c100c8acc41a765d46bf386f2
zxwanderer <no@mail.com>
2023-02-22 11:23:07 +0300

    updates

---
+++ libs/keyboard/scan_keys.asm
@@ -0,0 +1,20 @@
+	; принцип честно стырен из движка Wanderers by SamStyle
+	; в HL указатель на таблицу клавиш вида [SCANCODE_XXX] [BUTTON_XXX], 0 - признак окончания таблицы
+KEYBOARD_SCAN_KEYS:
+	XOR A
+	LD (KEYBOARD_PRESS_BUTTON), A
+scanKeys_loop:
+	LD A,(HL) ;//  загружаем первый байт
+	AND A  ;проверяем на 0
+	RET Z ; возвращаем если 0
+	INC HL ; увеличиваем HL
+	IN A,(0xFE) ; читаем значение
+	AND (HL) ; сравниваем со вторым байтом
+	INC HL
+	LD A, (HL); запомнили код
+	INC HL
+	JR NZ, scanKeys_loop
+	LD (KEYBOARD_PRESS_BUTTON), A
+	RET
+
+KEYBOARD_PRESS_BUTTON: defb 0
