236a043f33986498d90f88ee28670d7c7a106d6f
zxwanderer <38999246+zxwanderer@users.noreply.github.com>
2024-01-05 16:03:57 +0300

    Delete git-set-local.sh

--- git-set-local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-git config --local user.name "zxwanderer"
-git config --local user.email "no@mail.com"
-git config --local core.sshCommand 'ssh -i ~/.ssh/ed_wanderer -o IdentityAgent=none'
