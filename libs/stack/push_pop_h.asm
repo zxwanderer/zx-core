8d103ea57ec671c97b3a30e3b4057eb21305648b
zxwanderer <no@mail.com>
2024-02-02 20:42:13 +0300

    some updates

--- libs/stack/push_pop_h.asm
+++ libs/stack/push_pop_h.asm
@@ -1,11 +1,11 @@
   MACRO DO_PUSH_ALL_REGISTRY
-		push af,bc,de,hl,ix,iy
-		exx : ex af, af'
-		push af,bc,de,hl,ix,iy
+	push af,bc,de,hl,ix,iy
+	exx : ex af, af'
+	push af,bc,de,hl,ix,iy
   ENDM
 
   MACRO DO_POP_ALL_REGISTRY
-	  pop iy,ix,hl,de,bc,af
-		exx : ex af, af'
-		pop iy,ix,hl,de,bc,af
+	pop iy,ix,hl,de,bc,af
+	exx : ex af, af'
+	pop iy,ix,hl,de,bc,af
   ENDM
