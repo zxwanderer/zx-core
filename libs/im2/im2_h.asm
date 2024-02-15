6b9480be4fd2ddebe3b42b7b6fcedf65ee90c64c
zxwanderer <no@mail.com>
2024-02-15 09:51:39 +0300

    some updates

--- libs/im2/im2_h.asm
+++ libs/im2/im2_h.asm
@@ -1,7 +1,9 @@
 ; _intTab must be aligned 256 (#BC00)
 ; _vector must be high _vector = low _vector  (#BEBE)
 
-	MACRO SetIM2 _intTab, _vector
+	include "../stack/push_pop_h.asm"
+
+	MACRO SET_IM2 _intTab, _vector
 	  ld hl,_intTab
 	  ld de,_intTab+1
 	  ld bc,0x100
@@ -11,3 +13,15 @@
 	  ldir
 	  im 2
   	ENDM
+
+	MACRO IM2_ROUTINES_ORG_START _vector
+ORG _vector
+		DI
+		DO_PUSH_ALL_REGISTRY
+		ENDM
+
+	MACRO IM2_ROUTINES_END
+  	DO_POP_ALL_REGISTRY
+  	EI
+  	RET
+		ENDM
