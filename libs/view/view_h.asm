5ef25d5136f976a61cd4761ca0d4a5cde2d2ceed
zxwanderer <no@mail.com>
2024-02-16 20:36:05 +0300

    some updates

--- libs/view/view_h.asm
+++ libs/view/view_h.asm
@@ -1,4 +1,9 @@
+  ifndef _VIEW_H_
+  define _VIEW_H_
+
 VIEW_SCR_WINDOW_MAX_X equ TILE_MAP_SIZE_WIDTH-TILE_SCR_WIDTH+1  ; максимальная позиция окна отображения карты, иначе выходим за границу
 VIEW_SCR_WINDOW_MAX_Y equ TILE_MAP_SIZE_HEIGHT-TILE_SCR_HEIGTH+1 ; максимальная позиция окна отображения карты, иначе выходим за границу
 VIEW_SCR_WIDTH_HALF equ TILE_SCR_WIDTH/2;  8
 VIEW_SCR_HEIGHT_HALF equ TILE_SCR_HEIGTH/2; 6
+
+  endif
